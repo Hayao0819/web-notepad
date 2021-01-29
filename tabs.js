383c509e6919b57aef6e86714293df9db55068a8
hayao <hayao@fascode.net>
2021-01-29 19:37:26 +0900

    [update] : タブの追加を実装

--- tabs.js
+++ tabs.js
@@ -1,4 +1,4 @@
-var editors=[1,2];
+var editors=2;
 
 window.addEventListener("load", function(){
     enable_tab(1);
@@ -17,15 +17,28 @@ function enable_tab(number) {
     document.getElementById("tab-" + number).checked = true;
 
     // それ以外のエディターを削除
-    editors.forEach(
-        function(current){
-            if (current != number){
-                document.getElementById("editor-" + current + "-container").style.display = "none";
-            }
+    for (var current = 1; current < editors; current++){
+        if (current != number) {
+            document.getElementById("editor-" + current + "-container").style.display = "none";
         }
-    );
+    }
 
     // 設定値を更新
     current_tab = number;
     resize_editor();
+}
+
+function add_tab() {
+    // エディター数を更新
+    editors++;
+
+    // ボタンを生成
+    var tab_buttons = document.getElementById("tab-buttons");
+    var new_button  = `<label class="tab_item" for="tab-${editors}" onclick="enable_tab(${editors})"><input id="tab-${editors}" type="radio" name="tab_item">エディター${editors}</label>`;
+    tab_buttons.insertAdjacentHTML("beforeend", new_button);
+
+    // エディタを生成
+    var tab_editors = document.getElementById("tab-editors");
+    var new_editor  = `<div id="editor-${editors}-container"><form><input type="text" id="editor-${editors}-name"><br><textarea class="editor" id="editor-${editors}" placeholder="ここに入力"></textarea></form></div>`;
+    tab_editors.insertAdjacentHTML("beforeend", new_editor);
 }
